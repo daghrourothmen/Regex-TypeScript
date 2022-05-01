be03ff0ecf2bd5cd86d0c15238a529eb1ddd1ec6
daghrourothmen <daghrour@outlook.de>
2022-05-01 12:09:55 +0200

    Setup nodemon and typescript for testing regex formulas

---
+++ Test.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var Regex = "A regex formula 2";
+console.log(Regex);
+var o; //let can be declared and not initialized
+var y = "y"; //const must be always initialized
---
+++ Test.ts
@@ -0,0 +1,6 @@
+export {};
+let Regex= "A regex formula 2";
+console.log(Regex); 
+
+let o; //let can be declared and not initialized
+const y="y"; //const must be always initialized
