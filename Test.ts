10f14c1a72a15cca86f1719f07930e90ab68244a
daghrourothmen <daghrour@outlook.de>
2022-05-01 12:27:37 +0200

    Working with types

--- Test.ts
+++ Test.ts
@@ -4,3 +4,16 @@ console.log(Regex);
 
 let o; //let can be declared and not initialized
 const y="y"; //const must be always initialized
+
+//Types
+
+const ID:string ="s87f8s97g9s87ezf89ez798g";
+const First_Name:string="Othmen";
+const Last_Name:string="Daghrour";
+let Phone_Number:Number=96599024;
+let Job:string="JavaScript developer";
+const Rule:string="Front-end" || "Back-end";
+const isPaid:boolean=true;
+
+console.log(Rule);
+
